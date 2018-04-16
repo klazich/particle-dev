c874daaa347b4e0e880758cb2084b6fcaeed6611
Kevin Lazich <dev@kevinlazich.com>
2018-04-16 07:59:02 -0400

    chore: First build and test

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
+import Particle from 'particle-api-js';
+
 // promise composition helper
 const applyAsync = (acc, val) => acc.then(val);
 const composeAsync = (...funcs) => x =>
   funcs.reduce(applyAsync, Promise.resolve(x));
+
+const particle = new Particle();
+
+const loginOptions = {
+  username: process.env.LOGIN,
+  password: process.env.PASSWORD,
+};
+
+console.log(loginOptions);
+
+particle
+  .login(loginOptions)
+  .then(data => {
+    console.log('API call completed, data:', data);
+  })
+  .catch(err => {
+    console.log('API call failed with:', err);
+  });
