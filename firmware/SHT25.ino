735c1e2ca77017553f6ca75c2bbaded5feea09f1
Kevin Lazich <dev@kevinlazich.com>
2018-04-19 01:16:54 -0400

    testing: Tinkering around to learn the language

--- firmware/SHT25.ino
+++ firmware/SHT25.ino
@@ -11,21 +11,30 @@
 #define Addr 0x40
 
 double humidity = 0.0, cTemp = 0.0, fTemp = 0.0;
-void setup() 
+double humidityRunning = 0.0, cTempRunning = 0.0, fTempRunning = 0.0;
+double humidityAcc = 0.0, cTempAcc = 0.0, fTempAcc = 0.0;
+
+int count = 0;
+
+String curMsg;
+String runMsg;
+
+void setup()
 {
     // Set variable
     Particle.variable("i2cdevice", "SHT25");
     Particle.variable("humidity", humidity);
     Particle.variable("cTemp", cTemp);
-  
-    // Initialise I2C communication as MASTER 
+    Particle.variable("count", count);
+
+    // Initialise I2C communication as MASTER
     Wire.begin();
     // Initialise serial communication, set baud rate = 9600
     Serial.begin(9600);
     delay(300);
 }
 
-void loop() 
+void loop()
 {
     unsigned int data[2];
     // Start I2C communication
@@ -45,22 +54,19 @@ void loop()
     {
         data[0] = Wire.read();
         data[1] = Wire.read();
-        
+
         // Convert the data
-        humidity = ((((data[0] * 256.0) + data[1]) * 125.0) / 65536.0) - 6; 
-        
-        // Output data to dashboard
-        Particle.publish("Relative Humidity    :  ",  String(humidity));
+        humidity = ((((data[0] * 256.0) + data[1]) * 125.0) / 65536.0) - 6;
     }
 
     // Start I2C transmission
     Wire.beginTransmission(Addr);
-    // Send temperature measurement command, NO HOLD master           
+    // Send temperature measurement command, NO HOLD master
     Wire.write(0xF3);
     // Stop I2C transmission
     Wire.endTransmission();
     delay(500);
-    
+
     // Request 2 bytes of data
     Wire.requestFrom(Addr,2);
 
@@ -70,14 +76,44 @@ void loop()
     {
         data[0] = Wire.read();
         data[1] = Wire.read();
-        
+
         // Convert the data
         cTemp = ((((data[0] * 256.0) + data[1]) * 175.72) / 65536.0) - 46.85;
         fTemp = (cTemp * 1.8) + 32;
-        
-        // Output data to dashboard
-        Particle.publish("Temperature in Celsius:  ",  String(cTemp));
-        Particle.publish("Temperature in Fahrenheit:  ", String(fTemp));
-    } 
+    }
+
+    count += 1;
+
+    // parse data
+
+    curMsg = String(humidity) + ", " + String(cTemp) + ", " + String(fTemp);
+
+    humidityAcc += humidity;
+    cTempAcc += cTemp;
+    fTempAcc += fTemp;
+
+    humidityRunning = (humidityAcc / count);
+    cTempRunning = (cTempAcc / count);
+    fTempRunning = (fTempAcc / count);
+
+    runMsg = String(humidityRunning) + ", " + String(cTempRunning) + ", " + String(fTempRunning);
+
+    // Publish data
+
+    if ((count % 50) == 0)
+    {
+        Particle.publish("count", String(count) );
+        Particle.publish("RH, C, F", curMsg);
+        Particle.publish("3600 running avg", runMsg);
+
+        if ((count % 3600) == 0)
+        {
+            humidityAcc = 0.0;
+            cTempAcc = 0.0;
+            fTempAcc = 0.0;
+            count = 0;
+        }
+    }
+
   delay(300);
 }
